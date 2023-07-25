138a13ce39958896cd2aa75a751965798fd5ca43
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-25 15:09:06 +0300

    Update capitalize_decorator.rb

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,7 +1,7 @@
 require_relative 'decorator'
 
 class CapitalizeDecorator < Decorator
-    def correct_name
-        @nameable.correct_name.upcase
-    end
+  def correct_name
+    @nameable.correct_name.upcase
+  end
 end
