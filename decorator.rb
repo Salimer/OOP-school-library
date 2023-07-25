ca56069c559a98701236c29dfc7099b747e84e96
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-25 15:09:04 +0300

    Update decorator.rb

--- decorator.rb
+++ decorator.rb
@@ -1,11 +1,12 @@
 require_relative 'nameable'
 
 class Decorator < Nameable
-    def initialize(nameable)
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
