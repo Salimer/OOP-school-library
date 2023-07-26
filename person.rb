15da53c0f7338d89ad7d124c87f5dd622714109d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-26 14:38:28 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,16 +1,22 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..188)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self
+  end
 
   def can_use_services?
     of_age? || @parent_permission
