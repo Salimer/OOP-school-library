88acd702a2b65f0b5ece73d4c319dcaa5d509499
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-26 14:42:09 +0300

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,13 +1,13 @@
 require_relative 'book'
 
 class Rental
-    attr_accessor :date, :book, :person
+  attr_accessor :date, :book, :person
 
-    def initialize(date, book, person)
-        @date = date
-        @person = person
-        @book = book
-        person.add_rental(self) if person
-        book.add_rental(self) if book
-    end
+  def initialize(date, book, person)
+    @date = date
+    @person = person
+    @book = book
+    person&.add_rental(self)
+    book&.add_rental(self)
+  end
 end
