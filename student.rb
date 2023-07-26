15a2d3fa3d35ef2225f56b1200a08417ccf8c5f2
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-26 14:38:31 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -6,7 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom&.add_student(self)
+    classroom.add_student(self) if classroom
   end
 
   def play_hooky
