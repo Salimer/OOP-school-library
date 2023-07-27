c60ea8528f3f8b965aeab50d2e70f323513aa98e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-27 15:18:07 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -43,3 +43,18 @@ def invalid
   list_numbers
 end
 
+def list_numbers()
+  list = "
+Please choose an option by enterin a number (1-7):
+1 - List all books
+2 - List all people
+3 - Create a person
+4 - Create a book
+5 - Create a rental
+6 - List all rentals for a given person id
+7 - Exit
+"
+  puts list
+  gets.chomp
+end
+
