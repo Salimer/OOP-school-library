f5885bdc19749d17e8fe04daa706dde718075f9c
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-27 15:17:50 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -8,3 +8,14 @@ def list_all_people(app)
   list_numbers
 end
 
+def create_a_person(app)
+  print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+  choice = gets.chomp
+  print 'Age: '
+  age = gets.chomp
+  print 'Name: '
+  name = gets.chomp
+  app.create_a_person(choice, name, age)
+  list_numbers
+end
+
