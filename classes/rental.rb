a0c512dfb8338f44d586c3ffd8a951541e37dcb3
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-01 22:21:01 +0300

    Update rental.rb

--- classes/rental.rb
+++ classes/rental.rb
@@ -10,4 +10,12 @@ class Rental
     person&.add_rental(self)
     book&.add_rental(self)
   end
+
+  def to_hash
+    {
+      'date' => @date,
+      'person' => @person.to_hash,
+      'book' => @book.to_hash
+    }
+  end
 end
