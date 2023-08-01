dfa71ac44d3190acd28a76b607a7927db26f5af6
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-01 22:20:57 +0300

    Update teacher.rb

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -9,4 +9,16 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
+  def to_hash
+    {
+      'id' => @id,
+      'type' => self.class.name,
+      'name' => @name,
+      'age' => @age,
+      'specialization' => @specialization,
+      'parent_permission' => @parent_permission,
+      'rentals' => @rentals.map { |rental| { 'date' => rental.date } }
+    }
+  end
 end
