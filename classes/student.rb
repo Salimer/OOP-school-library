4e15523db4d0e2aa54b57ff7a1a08653f4ac1dd0
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-02 14:12:16 +0300

    Update student.rb

--- classes/student.rb
+++ classes/student.rb
@@ -3,8 +3,10 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, parent_permission, name = 'unknown')
-    super(age, name, parent_permission: parent_permission)
+  def initialize(classroom, age, parent_permission, name = 'unknown', id = nil)
+    id ||= Random.rand(1..188)
+
+    super(id, age, name, parent_permission: parent_permission)
     @classroom = classroom
     classroom&.add_student(self)
   end
