7844d4a5dcbee00c043e6cbf143a22cfb4608249
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-02 14:20:04 +0300

    Update storage.rb

--- modules/storage.rb
+++ modules/storage.rb
@@ -1,21 +1,19 @@
 require 'json'
 
 module Storage
-    def storage_directory_check?
-        Dir.exist?("storage")
-    end
+  def storage_directory_check?
+    Dir.exist?('storage')
+  end
 
-    def create_storage_directory
-        Dir.mkdir("storage")
-    end
+  def create_storage_directory
+    Dir.mkdir('storage')
+  end
 
-    def file_check?(name)
-        File.exists?("#{name}.json")
-    end
+  def file_check?(name)
+    File.exist?("#{name}.json")
+  end
 
-    def create_file(name, data)
-        File.open("storage/#{name}.json", "w") do |f|
-          f.write(JSON.pretty_generate(data))
-        end
-    end
+  def create_file(name, data)
+    File.write("storage/#{name}.json", JSON.pretty_generate(data))
+  end
 end
