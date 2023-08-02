ee46a63c597c3d77f28d3ff608fdacdf841e256b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-02 14:12:21 +0300

    Update person.rb

--- classes/person.rb
+++ classes/person.rb
@@ -4,9 +4,9 @@ class Person < Nameable
   attr_accessor :name, :age, :type
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(id, age, name = 'unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..188)
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
