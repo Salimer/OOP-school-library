34b9444fe8ef7daa9efa1ee272e7b121ed0c7af2
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 12:46:37 +0300

    Update capitalize_decorator_spec.rb

--- classes/spec/capitalize_decorator_spec.rb
+++ classes/spec/capitalize_decorator_spec.rb
@@ -1,13 +1,13 @@
-require_relative './spec_helper'
+require_relative 'spec_helper'
 
 describe Decorator do
-    before :each do
-        @capitalize_decorator = CapitalizeDecorator.new('Afghanistan')
-    end
+  before :each do
+    @capitalize_decorator = CapitalizeDecorator.new('Afghanistan')
+  end
 
-    context 'correct_name' do
-        it 'Check if nameable exists' do
-            expect(@capitalize_decorator.nameable).to eq 'Afghanistan'
-        end
+  context 'correct_name' do
+    it 'Check if nameable exists' do
+      expect(@capitalize_decorator.nameable).to eq 'Afghanistan'
     end
+  end
 end
