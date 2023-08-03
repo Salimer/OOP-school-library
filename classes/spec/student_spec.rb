4d2a01f279fa1ba8320648ba75c305a86fb57894
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 11:27:32 +0300

    Create student_spec.rb

---
+++ classes/spec/student_spec.rb
@@ -0,0 +1,23 @@
+# require_relative 'spec_helper'
+
+# describe Person do
+#   before :each do
+#     @classroom = Classroom.new('Math')
+#     @student = Student.new(@classroom, 10, true, 'Abdulali')
+#     @unknown = Person.new(10, 10)
+#   end
+
+#   context '#play_hooky' do 
+#     it "should return '¯(ツ)/¯'" do
+#       expect(@person).to be_an_instance_of Person
+#     end
+
+#     it 'should return the correct age = 30' do
+#       expect(@person.age).to eql 30
+#     end
+
+#     it 'should return the correct name = Abdulali' do
+#       expect(@person.name).to eql 'Abdulali'
+#     end
+#   end
+# end
