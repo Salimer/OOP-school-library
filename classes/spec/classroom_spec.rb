a62952a512f3bbdeb2baff453dc76dbce855098a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 12:46:35 +0300

    Update classroom_spec.rb

--- classes/spec/classroom_spec.rb
+++ classes/spec/classroom_spec.rb
@@ -1,15 +1,15 @@
-require_relative './spec_helper'
+require_relative 'spec_helper'
 
 describe Classroom do
-    before :each do
-        @classroom = Classroom.new('Math')
-        @student = Student.new(@classroom, 10, true, 'Abdulali')
-    end
+  before :each do
+    @classroom = Classroom.new('Math')
+    @student = Student.new(@classroom, 10, true, 'Abdulali')
+  end
 
-    context 'Add_student' do
-        it 'Check if a student is created inside the classroom class' do
-            @classroom.add_student(@student)
-            expect(@classroom.students[0]).to be_an_instance_of Student
-        end
+  context 'Add_student' do
+    it 'Check if a student is created inside the classroom class' do
+      @classroom.add_student(@student)
+      expect(@classroom.students[0]).to be_an_instance_of Student
     end
+  end
 end
