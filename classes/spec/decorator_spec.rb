889704a6d1105e32a01405d20f60152ccc569588
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 12:46:33 +0300

    Update decorator_spec.rb

--- classes/spec/decorator_spec.rb
+++ classes/spec/decorator_spec.rb
@@ -1,13 +1,13 @@
-require_relative './spec_helper'
+require_relative 'spec_helper'
 
 describe Decorator do
-    before :each do
-        @decorator = Decorator.new('Afghanistan')
-    end
+  before :each do
+    @decorator = Decorator.new('Afghanistan')
+  end
 
-    context 'correct_name' do
-        it 'Check if nameable exists' do
-            expect(@decorator.nameable).to eq 'Afghanistan'
-        end
+  context 'correct_name' do
+    it 'Check if nameable exists' do
+      expect(@decorator.nameable).to eq 'Afghanistan'
     end
+  end
 end
